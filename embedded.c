f222e3e962934a256dde60cf3759b3eab1314d65
Sajeer Shamsudeen <sajeer.shamsudeen@intel.com>
2019-08-15 12:57:14 -0700

    Added clock support to Port D

--- embedded.c
+++ embedded.c
@@ -6,10 +6,17 @@
 volatile uint32_t* const portd_moder   = (uint32_t*) 0x40020C00;
 volatile uint32_t* const portd_odr     = (uint32_t*) 0x40020C14;
 
+#ifndef CPM_2_0
+volatile uint32_t* const rcc_ahb1enr   = (uint32_t*) 0x40023830;
+#endif
+
 void sleep(uint32_t ms); // use systick to busy-wait
 
 int main(void)
 {
+#ifndef CPM_2_0
+  *rcc_ahb1enr |= (1 << 3);     // enable PortD's clock
+#endif
 
   uint32_t moder = *portd_moder;
   moder |= (1 << 16);
