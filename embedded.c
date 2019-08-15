8921caaa8c2443e35665cea279b6463d84cd2b22
Sajeer Shamsudeen <sajeer.shamsudeen@intel.com>
2019-08-15 12:41:54 -0700

    Embedded test code

---
+++ embedded.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdint.h>
+#include <time.h>
+
+volatile uint32_t* const portd_moder   = (uint32_t*) 0x40020C00;
+volatile uint32_t* const portd_odr     = (uint32_t*) 0x40020C14;
+
+void sleep(uint32_t ms); // use systick to busy-wait
+
+int main(void)
+{
+
+  volatile uint16_t a;
+  uint16_t volatile b;
+
+  volatile uint16_t *c;
+  uint16_t volatile *d;
+
+  uint32_t moder = *portd_moder;
+  moder |= (1 << 16);
+  moder &= ~(1 << 17);
+  *portd_moder = moder;
+
+  while(1) {
+    *portd_odr |= (1 << 8);   // led-on
+    sleep(500);
+    *portd_odr &= ~(1 << 8);  // led-off
+    sleep(500);
+  }
+}
+void sleep(uint32_t ms) // use systick to busy-wait
+{
+  long pause;
+  clock_t now,then;
+
+  pause = ms*(CLOCKS_PER_SEC/1000);
+  now = then = clock();
+  while( (now-then) < pause )
+      now = clock();
+}
